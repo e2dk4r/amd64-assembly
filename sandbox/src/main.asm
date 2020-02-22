0cb7e11c919d141c4a878a1bfca6643caabe7b8b
e2dk4r <3050229-e2dk4r@users.noreply.gitlab.com>
2020-02-22 13:04:17 +0300

    sandbox: refactor to be more moduler and understandable

--- sandbox/src/main.asm
+++ sandbox/src/main.asm
@@ -1,27 +1,34 @@
-section .data
-	msg:	db "First line", 0xa, \
-			   "Second line", 0xa, \
-			   "Third line", 0xa, 0x0,
-	msgsz:	equ	$-msg
+;===============================================================================
+; linux specific constants
+%define SYSOUT      1
+%define SYS_WRITE   1
+%define SYS_EXIT    60
 
-	SYSOUT:		equ 1
-	SYS_WRITE:	equ 1
-	SYS_EXIT:	equ 60
-	
-section .text
-	global _start
+;===============================================================================
+; section containing initialized data
+SECTION .data
+    ; message that will print out stdout
+    msg:    db "First line", 0xa,   \
+               "Second line", 0xa,  \
+               "Third line", 0xa, 0x0,
+    MSGLEN:  equ $-msg
+    
+;===============================================================================
+; section containing code
+SECTION .text
+GLOBAL _start
 
+;===============================================================================
+; MAIN PROGRAM
 _start:
-	; print string
-	mov		rdi, SYSOUT		; file discriptor
-	mov		rsi, msg		; msg address
-	mov		rdx, msgsz		; msg length
-	mov		rax, SYS_WRITE	; syscall code
-	syscall
+    ; print message
+    mov     rdi, SYSOUT     ; file descriptor 1: Standart Output
+    mov     rsi, msg        ; put message address
+    mov     rdx, MSGLEN     ; put message length 
+    mov     rax, SYS_WRITE  ; syscall code
+    syscall
 
-	; exit program
-	mov		rdi, 0			; status
-	mov		rax, SYS_EXIT	; syscall code
-	syscall
-
-;vi:ft=nasm
+    ; exit program
+    mov     rdi, 0          ; status
+    mov     rax, SYS_EXIT   ; syscall code
+    syscall
