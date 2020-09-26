fb13718d8d9b004e38ba538e206eee94ab9e1739
e2dk4r <3050229-e2dk4r@users.noreply.gitlab.com>
2020-09-26 16:05:41 +0300

    showargs: use copy of stack method to get arguments

--- showargs1/README.md
+++ showargs1/README.md
@@ -1,3 +1,5 @@
-# showargs1
+# showargs2
 A simple program in assembly for Linux, using NASM,
- demonstrating the way to access command line arguments on stack
+  demonstrating the way to access command line arguments on stack*
+
+*: creating an unchanging copy of the stack pointer by copying RSP into a 64-bit register as soon as program begins
--- showargs1/src/main.asm
+++ showargs1/src/main.asm
@@ -24,8 +24,6 @@ SECTION .bss
 ; you can comment out that line, keeping in mind that the arguments will not
 ; display correctly without EOL characters at their ends
     MAXARGS     equ 10          ; Maximum # of args we support
-    ArgCount:   resd 1          ; # of arguments passed to program
-    ArgPtrs:    resq MAXARGS    ; Table of pointers to arguments
     ArgLens:    resq MAXARGS    ; Table of argument lengths
     
 ;===============================================================================
@@ -36,26 +34,18 @@ GLOBAL _start
 ;===============================================================================
 ; MAIN PROGRAM
 _start:
-    ; get the command line argument count off the stack and validate it
-    pop     rcx                   ; TOS contains the argument count
-    cmp     rcx, MAXARGS          ; see if the arg count exceeds MAXARGS
+    mov     rbp, rsp              ; save the initial stack pointer in RBP
+
+    ; validate the command line argument count
+    cmp     qword [rbp], MAXARGS  ; see if the arg count exceeds MAXARGS
     ja      Error                 ; if so, exit with an error message
-    mov     dword [ArgCount], ecx ; save arg count in memory variable
 
-    ; once we know how many args we have, a loop will pop them into ArgPtrs:
-    xor     rdx, rdx                    ; zero loop counter
-SaveArgs:
-    pop     qword [ArgPtrs + rdx * 8]   ; pop an arg addr into the memory table
-    inc     rdx                         ; bump the counter to the next arg addr
-    cmp     rdx, rcx                    ; is the counter = the argument count?
-    jb      SaveArgs                    ; if not, loop back and do another
-
-    ; with the argument pointers stored in ArgPtrs, we calculate their lengths:
+    ; we calculate their lengths:
     xor     rax, rax        ; searching for 0, so clear AL to 0
     xor     rbx, rbx        ; argument counter - pointer table offset starts at 0
 ScanOne:
     mov     rcx, 0x0000ffff                 ; limit search to 65535 bytes max
-    mov     rdi, qword [ArgPtrs + rbx * 8]  ; put address of string to search in RDI
+    mov     rdi, qword [rbp + 8 + rbx * 8]  ; put address of string to search in RDI
     mov     rdx, rdi                        ; copy starting address into RDX
     cld                                     ; set search direction to up-memory
     repne scasb                             ; search for null(0 char) in string at RDI
@@ -64,20 +54,20 @@ ScanOne:
     sub     rdi, rdx                        ; subtract position of 0 from start address
     mov     qword [ArgLens + rbx * 8], rdi  ; put length of arg into table
     inc     rbx                             ; increment argument counter
-    cmp     ebx, [ArgCount]                 ; see if arg counter exceeds argument counter
+    cmp     rbx, [rbp]                      ; see if arg counter exceeds argument counter
     jb      ScanOne                         ; if not, loop back and do another one
 
     ; display all arguments to stdout
     xor     r8, r8          ; start (for table addressing reasons) at 0
 Showem:
     mov     rdx, [ArgLens + r8 * 8] ; argument length address
-    mov     rsi, [ArgPtrs + r8 * 8] ; argument address
+    mov     rsi, [rbp + 8 + r8 * 8] ; argument address
     mov     rdi, SYSOUT     ; file descriptor 1: Standart Output
     mov     rax, SYS_WRITE  ; syscall code
     syscall
 
     inc     r8              ; increment argument counter
-    cmp     r8d, [ArgCount] ; see if we've displayed all the arguments
+    cmp     r8, [rbp]       ; see if we've displayed all the arguments
     jb      Showem          ; if not, loop back and do another
 
     jmp     ExitS           ; we're done
